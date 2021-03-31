87aa7868c0890b406028065a5f021cdd984a579d
马晋 <13051152997@163.com>
2021-03-31 16:55:47 +0800

    学习 关系运算符

---
+++ lib/operator_relational.dart
@@ -0,0 +1,20 @@
+void main() {
+  int a = 1;
+  int b = 20;
+
+  print("判断a 和b 是否相等 ${a == b}");
+  print(a != b);
+  print(a > b);
+  print(a >= b);
+  print(a < b);
+  print(a <= b);
+
+  String stringA = "123";
+  String stringB = "321";
+  String stringC = "123";
+
+  print(stringA == stringB);
+  print(stringA == stringC);
+
+
+}
