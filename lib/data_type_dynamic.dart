006af79cfda1456bfd603559e5eca5d403fcf064
马晋 <13051152997@163.com>
2021-03-31 16:37:49 +0800

    学习 dynamic

---
+++ lib/data_type_dynamic.dart
@@ -0,0 +1,16 @@
+void main() {
+  var a;
+  a = 10;
+  a = "Dart";
+  print(a);
+
+  dynamic b = 20;
+  b = "JavaSrcipt";
+
+  var list = new List<dynamic>();
+  list.add(1);
+  list.add("hello");
+  list.add(true);
+  list.add(false);
+  print(list);
+}
