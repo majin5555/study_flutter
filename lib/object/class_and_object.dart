11e1b555c89b343d492d07f5e77e9cdce410a694
马晋 <13051152997@163.com>
2021-04-15 10:36:23 +0800

    学习  类与对象

---
+++ lib/object/class_and_object.dart
@@ -0,0 +1,13 @@
+import 'person.dart';
+
+void main() {
+  var person = Person();
+  person.name = "小明";
+  person.age = 28;
+
+  person.work();
+}
+
+
+
+}
---
+++ lib/object/person.dart
@@ -0,0 +1,15 @@
+class Person {
+  String _name;//前边加下划线 代表私有性
+  String name;
+  int age;
+  //不可变的
+  final String address="北京";
+
+  void work() {
+    print("Name is $name ,Age is $age ,He is working...");
+  }
+
+//dart 中 方法不能被重载
+// void work(){
+//
+// }
