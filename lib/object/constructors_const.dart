09386f13d007874d30777baa99a89233c726a62e
马晋 <13051152997@163.com>
2021-04-15 14:15:25 +0800

    学习 常量构造方法

--- lib/object/constructors_const.dart
+++ lib/object/constructors_const.dart
@@ -1,20 +1,19 @@
 /**
  * @author: Majin
  * @date: 2021/4/15
- * @desc: 构造方法
+ * @desc: 常量构造方法
  */
 void main() {
-  var person = Person("小明", 1, "aaa");
-  person.name = "小明";
-  person.age = 28;
+  const person = Person("小明", 1, "aaa");
+  person.work();
 }
 
 class Person {
-  String name;
-  int age;
+  final String name;
+  final int age;
   final String gender;
 
-  Person(this.name, this.age, this.gender);
+  const Person(this.name, this.age, this.gender);
 
   //与上边相同
   // Person(this.name, this.age);
