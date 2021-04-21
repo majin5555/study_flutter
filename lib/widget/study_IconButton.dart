9f351e224c9798760d38e48942fee47171da7ed3
马晋 <13051152997@163.com>
2021-04-21 10:01:48 +0800

    学习 IconButton 控件（图标按钮）

---
+++ lib/widget/study_IconButton.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+/**
+ * @author: Majin
+ * @date: 2021/4/20
+ * @desc:   IconButton  按钮组件
+ */
+
+class StudyIconButton extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Study IconButton',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("AppBar Study IconButton"),
+        ),
+        body: Center(
+          child: Container(
+            width: 200,
+            decoration: BoxDecoration(color: Colors.red),
+            height: 200,
+            child: IconButton(
+              iconSize: 100,
+              icon: Icon(Icons.print),
+              color: Colors.green,
+              tooltip: "提示文本",
+              //根据父容器来决定图标的位置
+              alignment: AlignmentDirectional.center,
+              onPressed: () {
+                print("onPressed-------------");
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
