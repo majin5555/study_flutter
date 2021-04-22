2191a5133cb0585c96cad3854004ab261c069a6c
马晋 <13051152997@163.com>
2021-04-22 15:07:36 +0800

    学习 ImageFile(图片显示组件--图片选择器)

---
+++ lib/widget/study_Filemage.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+/**
+ * @author: Majin
+ * @date: 2021/4/21
+ * @desc:   ImageFile   资源图片
+ */
+
+class StudyImageFile extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Study ImageFile',
+      color: Colors.orange,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("AppBar ImageFile 组件"),
+        ),
+        body: DemoPage(),
+      ),
+    );
+  }
+}
+
+class DemoPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => DemoPageDemo();
+}
+
+class DemoPageDemo extends State<DemoPage> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Center(
+            child: _image == null
+                ? Text("你还未选择任何图片？")
+                : Image.file(
+                    _image,
+                    scale: 9,
+                    fit: BoxFit.scaleDown,
+                  ),
+          ),
+          RaisedButton(
+            onPressed: getImage,
+            child: Text("点击选择图片"),
+          ),
+        ],
+      ),
+    );
+  }
+}
